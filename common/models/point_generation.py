e55280e9061a5206d43e8b542c48d6ae01b30a24
Artyom Vyatkin <vyatkin.artex@gmail.com>
2021-09-26 19:46:48 +0300

    added poin generators

--- common/models/point_generation.py
+++ common/models/point_generation.py
@@ -1,20 +1,26 @@
 from abc import ABC, abstractmethod
+from random import uniform
 from typing import List
 
+from common.models.float_with_accuracy import FloatWithAccuracy
 from common.models.line_segment import LineSegment
 
 
 class PointGenerator(ABC):
     @abstractmethod
-    def generate(self, line_segment: LineSegment, number_of_points: int) -> List[int]:
+    def generate(self, line_segment: LineSegment, number_of_points: int) -> List[float]:
         pass
 
 
-#
-# class RandomPointGenerator(PointGenerator):
-#
-#
-#
+class RandomPointGenerator(PointGenerator):
+    def generate(self, line_segment: LineSegment, number_of_points: int) -> List[float]:
+        points = set()
+        while len(points) < number_of_points:
+            new_value = FloatWithAccuracy(uniform(line_segment.left, line_segment.right))
+            points.add(new_value)
+        return [float(val) for val in points]
+
+
 class EquidistantPointGenerator(PointGenerator):
-    def generate(self, line_segment: LineSegment, number_of_points: int) -> List[int]:
+    def generate(self, line_segment: LineSegment, number_of_points: int) -> List[float]:
         return line_segment.split_into_points(number_of_points)
