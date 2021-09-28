95d40697ed6a9bd3f3f977ee6d05aadd4d94a64d
Artyom Vyatkin <vyatkin.artex@gmail.com>
2021-09-28 20:33:52 +0300

    made config constants

--- tasks/task2/main.py
+++ tasks/task2/main.py
@@ -1,19 +1,19 @@
 import sys
 
+from tasks.task2.config import INTERPOLATORS, POINT_GENERATORS
+
 sys.path.append("")
 sys.path.append("../..")
 
-from sympy.parsing.sympy_parser import parse_expr
 from sympy import lambdify
 
 from common.models.line_segment import LineSegment
 from common.models.point_generation import RandomPointGenerator, EquidistantPointGenerator
 from config import THEME_COLOR
 from tasks.utils.plotly_utils import update_figure_to_x_axis
-from tasks.utils.expression_parsing import process_expression
+from tasks.utils.expression_parsing import custom_parse_expr
 from common.calculation.interpolation.find_optimal_points import find_optimal_points
 from common.calculation.interpolation.interpolators.lagrangian_interpolator import LagrangianInterpolator
-from common.calculation.interpolation.interpolators.newton_interpolator import NewtonInterpolator
 import numpy as np
 import streamlit as st
 import plotly.graph_objects as go
@@ -86,20 +86,11 @@ def display_result(func, x, approximate_value, points, interpolator_name):
 
 
 def main():
-    interpolators = {
-        LagrangianInterpolator.name: LagrangianInterpolator(),
-        NewtonInterpolator.name: NewtonInterpolator(),
-    }
-    point_generators = {
-        RandomPointGenerator.name: RandomPointGenerator(),
-        EquidistantPointGenerator.name: EquidistantPointGenerator(),
-    }
-
     if "seed" not in st.session_state:
         st.session_state["seed"] = 666
         st.session_state["number_of_points"] = 15
 
-    func = lambdify("x", parse_expr(process_expression(FUNC_EXPRESSION)))
+    func = lambdify("x", custom_parse_expr(FUNC_EXPRESSION))
 
     st.title("Algebraic interpolation")
     st.markdown(rf"""Function to interpolate: $\;$ ${FUNC_EXPRESSION}$""")
@@ -109,7 +100,7 @@ def main():
     if number_of_all_points != st.session_state["number_of_points"]:
         st.session_state["number_of_points"] = number_of_all_points
         st.session_state["seed"] += 1
-        point_generators[RandomPointGenerator.name].seed = st.session_state["seed"]
+        POINT_GENERATORS[RandomPointGenerator.name].seed = st.session_state["seed"]
 
     segment_col1, segment_col2 = st.columns(2)
     left_bound = segment_col1.number_input("Enter segment boundaries", step=0.1, value=0.0)
@@ -117,10 +108,10 @@ def main():
 
     line_segment = LineSegment(left_bound, right_bound)
 
-    point_generator_name = st.selectbox("Choose how to generate points", tuple(point_generators))
+    point_generator_name = st.selectbox("Choose how to generate points", tuple(POINT_GENERATORS))
 
     if number_of_all_points is not None:
-        all_points = sorted(point_generators[point_generator_name].generate(line_segment, number_of_all_points))
+        all_points = sorted(POINT_GENERATORS[point_generator_name].generate(line_segment, number_of_all_points))
         st.markdown(f"${LINE_START[1:-1] * 3}$ **Generated points**")
         display_points(all_points)
         x_node = st.number_input("Enter interpolation point x:", step=0.1, value=0.35)
@@ -135,12 +126,12 @@ def main():
             optimal_points = find_optimal_points(x_node, all_points, polynomial_degree + 1)
             st.markdown(f"${LINE_START[1:-1] * 3}$ **Optimal points**")
             display_points(optimal_points)
-            interpolator_name = st.selectbox("Choose interpolator", tuple(interpolators))
+            interpolator_name = st.selectbox("Choose interpolator", tuple(INTERPOLATORS))
             value_table = (optimal_points, [func(point) for point in optimal_points])
             display_result(
                 func,
                 x_node,
-                interpolators[interpolator_name].get_approximate_value(x_node, value_table),
+                INTERPOLATORS[interpolator_name].get_approximate_value(x_node, value_table),
                 optimal_points,
                 interpolator_name,
             )
