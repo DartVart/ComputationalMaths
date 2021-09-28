2a9f54ef2580c4decd3a9a9a483a3f9b0c92c2bb
Artyom Vyatkin <vyatkin.artex@gmail.com>
2021-09-28 20:32:32 +0300

    changed parsing expression

--- tasks/utils/expression_parsing.py
+++ tasks/utils/expression_parsing.py
@@ -1,2 +1,19 @@
-def process_expression(expression: str) -> str:
-    return expression.replace("^", "**")
+from sympy.parsing.sympy_parser import parse_expr
+from sympy.parsing.sympy_parser import (
+    standard_transformations,
+    split_symbols,
+    implicit_multiplication,
+    function_exponentiation,
+    convert_xor,
+)
+
+TRANSFORMATIONS = standard_transformations + (
+    split_symbols,
+    implicit_multiplication,
+    function_exponentiation,
+    convert_xor
+)
+
+
+def custom_parse_expr(expression):
+    return parse_expr(expression, transformations=TRANSFORMATIONS)
